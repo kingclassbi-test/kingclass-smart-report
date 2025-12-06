850704132c9fc30b6a50b8857bd44096fdd999d9
kingclassbi-test <kingclass.bi@gmail.com>
2025-12-06 18:36:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import pandas as pd
+import streamlit as st
 
-# 1) โหลด Branch List
-sheet_id = "1mDVLSD2VWvIEX3pr68hdntZYtqeO7IQZXopvyLEeM6E"
-url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv"
+# --- Load Branch List Sheet ---
+branch_sheet_id = "1mDVLSD2VWvIEX3pr68hdntZYtqeO7IQZXopvyLEeM6E"
+branch_url = f"https://docs.google.com/spreadsheets/d/{branch_sheet_id}/gviz/tq?tqx=out:csv&sheet=Sheet1"
 
-branch_df = pd.read_csv(url)
+branch_df = pd.read_csv(branch_url)
 
-branch_df
+st.header("📍 เลือกสาขา")
+
+branch_list = branch_df["Branch_Name"].tolist()
+selected_branch = st.selectbox("เลือกสาขา", branch_list)
+
+st.write("คุณเลือกสาขา:", selected_branch)
